e6c3d5d85c1b75bca59c767894fd6b2d8a7adf56
atman-33 <gpbjk0304@gmail.com>
2025-07-03 09:02:35 +0900

    feat: add Google Adsense and monetag scripts for advertisement integration

--- app/root.tsx
+++ app/root.tsx
@@ -74,17 +74,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
         }
       `}
         </script>
+        {/* Googler Adsense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3632222360837456"
           crossOrigin="anonymous"
         ></script>
+
+        {/* monetag - Vignette Banner  */}
         <script
-          src="https://fpyf8.com/88/tag.min.js"
-          data-zone="155108"
-          async
-          data-cfasync="false"
-        ></script>
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
+          dangerouslySetInnerHTML={{
+            __html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9519893,document.createElement('script'))`,
+          }}
+        />
+        {/* monetag - In-Page Push  */}
+        <script
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
+          dangerouslySetInnerHTML={{
+            __html: `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9519899,document.createElement('script'))`,
+          }}
+        />
         <Meta />
         <Links />
       </head>
