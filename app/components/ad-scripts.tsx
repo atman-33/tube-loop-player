73d1f3c2d48da85123218878ce5c7ecdb107930e
atman-33 <gpbjk0304@gmail.com>
2025-07-05 16:51:09 +0900

    feat: rearrange monetag script comments for better organization

--- app/components/ad-scripts.tsx
+++ app/components/ad-scripts.tsx
@@ -7,21 +7,6 @@ const AdScripts = () => {
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3632222360837456"
         crossOrigin="anonymous"
       ></script>
-
-      {/* monetag - Vignette Banner  */}
-      <script
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
-        dangerouslySetInnerHTML={{
-          __html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9519893,document.createElement('script'))`,
-        }}
-      />
-      {/* monetag - In-Page Push  */}
-      {/* <script
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
-        dangerouslySetInnerHTML={{
-          __html: `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9519899,document.createElement('script'))`,
-        }}
-      /> */}
       {/* Google tag (gtag.js) */}
       <script
         async
@@ -36,6 +21,21 @@ const AdScripts = () => {
           gtag('config', 'G-1R35RBBGXL');
         `}
       </script>
+
+      {/* monetag - Vignette Banner  */}
+      {/* <script
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
+        dangerouslySetInnerHTML={{
+          __html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9519893,document.createElement('script'))`,
+        }}
+      /> */}
+      {/* monetag - In-Page Push  */}
+      {/* <script
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: Embedding external advertisement script
+        dangerouslySetInnerHTML={{
+          __html: `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9519899,document.createElement('script'))`,
+        }}
+      /> */}
     </>
   );
 };
