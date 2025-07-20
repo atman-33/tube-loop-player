c858136a1626be10803f36e7a07448278b454d79
atman-33 <gpbjk0304@gmail.com>
2025-07-20 23:25:07 +0900

    feat: delete ad toaster

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router';
 import { Toaster } from '~/components/ui/sonner';
-import { useAdToaster } from '~/hooks/use-ad-toaster';
 import Footer from '~/routes/_app/components/footer';
 import type { Route } from './+types/route';
 import Header from './components/header';
@@ -12,7 +11,6 @@ export const loader = async ({ context }: Route.LoaderArgs) => {
 
 const AppLayout = ({ loaderData }: Route.ComponentProps) => {
   const { contactEmail } = loaderData;
-  useAdToaster(); // Call the custom hook here
   return (
     <>
       <div className="container mx-auto p-4">
